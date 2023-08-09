8958b8b7cd2927136b095ea3c34c5c70ccff2728
novaotp <rahmansajidur30@gmail.com>
2023-08-09 08:34:47 +0200

    Removed node modules + added gitignore file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
 const path = require('path');
-const glob = require('glob')
 const TerserPlugin = require('terser-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
 
 module.exports = {
   entry: './src/app/index.js',
@@ -13,7 +9,6 @@ module.exports = {
   },
   module: {
     rules: [
-      // ... other rules
       {
         test: /\.html$/,
         use: ['html-loader']
@@ -43,27 +38,4 @@ module.exports = {
       extractComments: false,  // Do not extract comments to a separate file
     })],
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-    new PurgeCSSPlugin({
-      paths: glob.sync(`${path.join(__dirname, "./src")}/**/*`, { nodir: true }),
-    }),
-    new HtmlWebpackPlugin({
-        template: './src/app/index.html',
-        minify: {
-            collapseWhitespace: true,
-            removeComments: true,
-            removeRedundantAttributes: true,
-            useShortDoctype: true,
-            removeEmptyAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            keepClosingSlash: true,
-            minifyJS: true,
-            minifyCSS: true,
-            minifyURLs: true
-        }
-    })
-]
 };
